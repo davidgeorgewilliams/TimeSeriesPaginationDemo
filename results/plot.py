ff9c995f7fd0727ee0bfd1e3edb92ddbc7873178
David Williams <david@davidgeorgewilliams.com>
2021-04-14 08:47:15 -0700

    Add score plots.

--- results/plot.py
+++ results/plot.py
@@ -7,8 +7,8 @@ plt.style.use('dark_background')
 constant_df = pd.read_csv("data/constantStrategy.csv")
 qlearning_df = pd.read_csv("data/qLearningStrategy.csv")
 
-plt.title('Score = delivered - discarded')
-plt.ylabel('Score')
+plt.title('Reward = delivered - discarded')
+plt.ylabel('Reward')
 plt.xlabel('Iteration')
 
 constant_rolling = constant_df.rolling(window=100).mean()
@@ -21,7 +21,7 @@ plt.xticks(x_ticks)
 plt.plot(constant_rolling["score"])
 plt.plot(qlearning_rolling["score"])
 
-plt.legend(['Constant', 'QLearning'], loc='upper center', bbox_to_anchor=(0.5, -0.20), ncol=2)
+plt.legend(['Constant', 'Q-Learning'], loc='upper center', bbox_to_anchor=(0.5, -0.20), ncol=2)
 plt.tight_layout(pad=1)
 
 plt.savefig(f"scores.png", dpi=400)
